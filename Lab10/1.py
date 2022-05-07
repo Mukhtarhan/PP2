f6d93b71d65a63ad6b8ddc15ba4c9147200a2f61
mukhtarhan <98650261+Mukhtarhan@users.noreply.github.com>
2022-05-07 17:13:54 +0600

    Add files via upload

--- Lab10/1.py
+++ Lab10/1.py
@@ -1,37 +1,22 @@
-from configparser import ConfigParser
-
 import psycopg2
 
+config = psycopg2.connect(
+    host='localhost',
+    database='postgres',
+    port='5432',
+    user='postgres',
+    password='muha2003'
+)
 
-def config(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
+current=config.cursor()
 
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
-    return db
+current.execute('''INSERT INTO phonebook (name, phone) VALUES( 
+    'Mukhtarhan','87052885795'), 
+    ('Sabyrjan','77075996255')'''
+    )
+config.commit()
+current.execute('SELECT * from phonebook')
+records=current.fetchall()
 
-conn = None
-try:
-    params = config()
-    conn = psycopg2.connect(**params)
-    cur = conn.cursor()
-    cur.execute('SELECT version()')
-    result = cur.fetchone()
-    print(result)
-    cur.close()
-except(Exception, psycopg2.DatabaseError) as error:
-    print(error)
-finally:
-    if conn is not None:
-        conn.close()
-        print("connection closed")
+print(records)
